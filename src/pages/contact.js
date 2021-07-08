0135b38eced95b08f4c05b8ca05c760075b018c7
Santiago548 <dominicbarajas548@gmail.com>
2021-07-07 21:17:02 -0600

    added all social logos and working links to contact me.

---
+++ src/pages/contact.js
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import {
+    socialContainer,
+    socialLink,
+    fieldsetSocial,
+  } from "../components/layout.module.css";
+
+const ContactPage = () => {
+    return (
+        <div className={socialContainer}>
+        <a href="https://www.linkedin.com/in/dominic-barajas/" target="_blank">
+          <fieldset className={fieldsetSocial}>
+            <StaticImage className={socialLink} alt="linkedin" src="../images/linkedin_logo.png" />
+          </fieldset>
+        </a>
+        <a href="mailto: dominicbarajas548@gmail.com" target="_blank">
+            <fieldset className={fieldsetSocial}>
+                <StaticImage className={socialLink} alt="gmail" src="../images/gmail_logo.png" />
+            </fieldset>
+        </a>
+        <a href="https://www.instagram.com/domsantiagobarajas/" target="_blank">
+        <fieldset className={fieldsetSocial}>
+            <StaticImage className={socialLink} alt="instagram" src="../images/instagram_logo.png" />
+        </fieldset>
+        </a>
+        <br></br>
+        <a href="https://twitter.com/TechDom2" target="_blank">
+        <fieldset className={fieldsetSocial}>
+
+            <StaticImage className={socialLink} alt="twitter" src="../images/twitter_logo.png" />
+        </fieldset>
+        </a>
+        <a href="https://dev.to/santiago548" target="_blank">
+        <fieldset className={fieldsetSocial}>
+            <StaticImage className={socialLink} alt="dev" src="../images/dev_logo.png" />
+        </fieldset>
+        </a>
+        <a href="https://www.twitch.tv/santiago5480" target="_blank">
+        <fieldset className={fieldsetSocial}>
+            <StaticImage className={socialLink} alt="twitch" src="../images/twitch_logo.png" />
+        </fieldset>
+        </a>
+        {/* <a href="https://www.linkedin.com/in/dominic-barajas/" target="_blank"> 
+            <StaticImage className={socialLink} alt="discord" src="../images/discord_logo.png" />
+        </a> */}
+      </div>
+    );
+  };
+  
+  export default ContactPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import ContactPage from './contact.js'
 import { StaticImage } from 'gatsby-plugin-image'
 import { profilePic } from "../components/layout.module.css";
 import '../fonts/Disket-Mono-Bold.ttf'
@@ -7,14 +8,20 @@ import '../fonts/Disket-Mono-Regular.ttf'
 
 const IndexPage = () => {
   return (
+    <div>
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
        alt="PROFILE PIC"
        className={profilePic}
        src="../images/profile_pic.jpg"
-      />
+       />
+       <br></br>
+    <h1>Contact Me</h1>
     </Layout>
+    <br></br>
+<ContactPage />
+    </div>
   )
 }
 export default IndexPage
