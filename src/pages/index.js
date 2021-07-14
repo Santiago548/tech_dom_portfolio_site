db476375f83a5d529f7738a63101454b3d736d75
Santiago548 <dominicbarajas548@gmail.com>
2021-07-14 10:21:22 -0600

    added opening description to home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,39 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import ContactPage from './contact.js'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from "react";
+import Layout from "../components/layout";
+import ContactPage from "./contact.js";
+import { StaticImage } from "gatsby-plugin-image";
 import { profilePic } from "../components/layout.module.css";
-import '../fonts/Disket-Mono-Bold.ttf'
-import '../fonts/Disket-Mono-Regular.ttf'
+import "../fonts/Disket-Mono-Bold.ttf";
+import "../fonts/Disket-Mono-Regular.ttf";
 
 const IndexPage = () => {
   return (
     <div>
-    <Layout pageTitle="Home Page">
-      <p>welcome to my site. </p>
-      <StaticImage
-       alt="PROFILE PIC"
-       className={profilePic}
-       src="../images/profile_pic.jpg"
-       />
-       <br></br>
-    <h1>Connect With Me</h1>
-    </Layout>
-    <br></br>
-<ContactPage />
+      <Layout pageTitle="Home Page">
+        <p>
+          My name is Dominic and welcome to my site. I'm constantly expanding my
+          knowledge as a Full-Stack Web Developer by building applications and
+          websites. From owning a parkour gym and coaching students to drafting
+          Civil 3D models, I have held a variety of positions. I saw every
+          obstacle as an opportunity to learn and improve as a parkour
+          practitioner, and I applied those skills of adaptability and
+          problem-solving to my programming. There has yet to be a problem I
+          haven't been able to solve with my research. As a result of my
+          adaptability, I have acquired many programming and web skills.
+        </p>
+        <StaticImage
+          alt="PROFILE PIC"
+          className={profilePic}
+          src="../images/profile_pic.jpg"
+        />
+        <br></br>
+        <h1>Connect With Me</h1>
+      </Layout>
+      <br></br>
+      <ContactPage />
     </div>
-  )
-}
-export default IndexPage
+  );
+};
+export default IndexPage;
 
 // localhost:8000/___graphql
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,24 +4,17 @@ import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import {
   projectContainer,
-  npcImage,
-  storyImage,
-  characterImage,
   npcLink,
   characterLink,
   storyLink,
-  managerImage,
   managerLink,
   projectNoFieldset,
   fieldsetLink,
-  npcDescription,
-  managerDescription,
-  characterDescription,
   storyDescription,
   fieldsetProject1,
   fieldsetProject2,
   projectDescription,
-  fieldsetProjectContainer
+  fieldsetProjectContainer,
 } from "../components/layout.module.css";
 
 const ProjectPage = () => {
@@ -30,125 +23,127 @@ const ProjectPage = () => {
       <p>All my projects will be here.</p>
       <div className={projectContainer}>
         <fieldset className={fieldsetProjectContainer}>
-        <fieldset className={fieldsetProject1}>
-          <StaticImage alt="npc logo" src="../images/npc_logo.png" />
-          <br />
-          <p className={projectDescription}>
-            Version 1.0 of a quick Dungeons and Dragons 5e NPC generator for
-            making randomly generated NPCs for up to three encounters on the
-            fly.
-          </p>
-          <fieldset className={projectNoFieldset}>
-            <Link
-              to="https://github.com/Santiago548/NPC_Generator"
-              className={npcLink}
+          <fieldset className={fieldsetProject1}>
+            <StaticImage alt="npc logo" src="../images/npc_logo.png" />
+            <br />
+            <p className={projectDescription}>
+              You can generate random NPCs on the fly with Health, Armor Class,
+              Alignment, Weapons, and Ability scores for Dungeons and Dragons
+              5e. With the click of a button.
+            </p>
+            <fieldset className={projectNoFieldset}>
+              <Link
+                to="https://github.com/Santiago548/NPC_Generator"
+                className={npcLink}
               >
-              <fieldset className={fieldsetLink}>GITHUB</fieldset>
-            </Link>
-            <Link
-              to="https://github.com/Santiago548/NPC_Generator"
-              className={npcLink}
+                <fieldset className={fieldsetLink}>GITHUB</fieldset>
+              </Link>
+              <Link
+                to="https://github.com/Santiago548/NPC_Generator"
+                className={npcLink}
               >
-              <fieldset className={fieldsetLink}>
-                DEMO need to add video
-              </fieldset>
-            </Link>
+                <fieldset className={fieldsetLink}>
+                  DEMO need to add video
+                </fieldset>
+              </Link>
+            </fieldset>
           </fieldset>
-        </fieldset>
-        {/* character manager links */}
-        <fieldset className={fieldsetProject2}>
-          <StaticImage
-            alt="character manager logo"
-            src="../images/character_manager.png"
+          {/* character manager links */}
+          <fieldset className={fieldsetProject2}>
+            <StaticImage
+              alt="character manager logo"
+              src="../images/character_manager.png"
             />
-          <br />
-          <p className={projectDescription}>
-            A character manager with a user sign-up and login capability to
-            store characters for a D&D campaign. with validations and errors to
-            not allow others to modify other characters
-          </p>
-          <fieldset className={projectNoFieldset}>
-            <Link
-              href="https://github.com/Santiago548/DND_character_manager"
-              className={characterLink}
+            <br />
+            <p className={projectDescription}>
+              Character manager that allows users to register and login to store
+              characters for D&D campaigns. Verifications and errors are used to
+              prevent others from editing other characters
+            </p>
+            <fieldset className={projectNoFieldset}>
+              <Link
+                href="https://github.com/Santiago548/DND_character_manager"
+                className={characterLink}
               >
-              <fieldset className={fieldsetLink}>GITHUB</fieldset>
-            </Link>
-            <Link
-              to="https://github.com/Santiago548/NPC_Generator"
-              className={characterLink}
+                <fieldset className={fieldsetLink}>GITHUB</fieldset>
+              </Link>
+              <Link
+                to="https://github.com/Santiago548/NPC_Generator"
+                className={characterLink}
               >
-              <fieldset className={fieldsetLink}>
-                DEMO need to add video
-              </fieldset>
-            </Link>
-              </fieldset>
+                <fieldset className={fieldsetLink}>
+                  DEMO need to add video
+                </fieldset>
+              </Link>
+            </fieldset>
           </fieldset>
         </fieldset>
-       <br />
-  
+        <br />
+
         {/* NPC generator links */}
         <fieldset className={fieldsetProjectContainer}>
-        <fieldset className={fieldsetProject1}>
-          <StaticImage
-            alt="story generator logo"
-            src="../images/character_generator.png"
-          />
-          <br />
-          <p className={storyDescription}>
-            an application to quickly create Characters for RPGs or for writing
-            for stories. can use the form to make a character card or can use
-            the generator button to create characters for three different
-            genres. The genres include general, sci-fi, and fantasy.
-          </p>
-          <fieldset className={projectNoFieldset}>
-            <Link
-              to="https://github.com/Santiago548/Character_APP"
-              className={storyLink}
-            >
-              <br />
-              <fieldset className={fieldsetLink}>GITHUB</fieldset>
-            </Link>
-            <Link
-              to="https://github.com/Santiago548/NPC_Generator"
-              className={storyLink}
-            >
-              <fieldset className={fieldsetLink}>
-                DEMO need to add video
-              </fieldset>
-            </Link>
+          <fieldset className={fieldsetProject1}>
+            <StaticImage
+              alt="story generator logo"
+              src="../images/character_generator.png"
+            />
+            <br />
+            <p className={storyDescription}>
+              With this application, you can quickly construct characters for
+              use in role-playing games and writing stories. You can create
+              character cards using the forms, or use the character generator to
+              generate characters randomly from one of three genres. Genres
+              include fantasy, sci-fi, and general fiction.
+            </p>
+            <fieldset className={projectNoFieldset}>
+              <Link
+                to="https://github.com/Santiago548/Character_APP"
+                className={storyLink}
+              >
+                <br />
+                <fieldset className={fieldsetLink}>GITHUB</fieldset>
+              </Link>
+              <Link
+                to="https://github.com/Santiago548/NPC_Generator"
+                className={storyLink}
+              >
+                <fieldset className={fieldsetLink}>
+                  DEMO need to add video
+                </fieldset>
+              </Link>
+            </fieldset>
           </fieldset>
-        </fieldset>
 
-        {/*  campaign manager links */}
-        <fieldset className={fieldsetProject2}>
-          <StaticImage
-            alt="campaign manager logo"
-            src="../images/campaign_manager.png"
-          />
-          <br />
-          <p className={projectDescription}>
-            Application with the ability for users to sign up and log in to
-            store campaign information for players to create characters and
-            dungeon masters to create campaigns.
-          </p>
-          <fieldset className={projectNoFieldset}>
-            <Link
-              to="https://github.com/Santiago548/gm_campaign_app"
-              className={managerLink}
-            >
-              <fieldset className={fieldsetLink}>GITHUB</fieldset>
-            </Link>
-            <Link
-              to="https://github.com/Santiago548/NPC_Generator"
-              className={managerLink}
-            >
-              <fieldset className={fieldsetLink}>
-                DEMO need to add video
-              </fieldset>
-            </Link>
+          {/*  campaign manager links */}
+          <fieldset className={fieldsetProject2}>
+            <StaticImage
+              alt="campaign manager logo"
+              src="../images/campaign_manager.png"
+            />
+            <br />
+            <p className={projectDescription}>
+              Users can register either as a player or as a Game Master. The
+              player has the option of creating and storing characters, while
+              the Game Master has the option of storing information about
+              campaigns and creating characters for other players.
+            </p>
+            <fieldset className={projectNoFieldset}>
+              <Link
+                to="https://github.com/Santiago548/gm_campaign_app"
+                className={managerLink}
+              >
+                <fieldset className={fieldsetLink}>GITHUB</fieldset>
+              </Link>
+              <Link
+                to="https://github.com/Santiago548/NPC_Generator"
+                className={managerLink}
+              >
+                <fieldset className={fieldsetLink}>
+                  DEMO need to add video
+                </fieldset>
+              </Link>
+            </fieldset>
           </fieldset>
-        </fieldset>
         </fieldset>
       </div>
     </Layout>
