f44d2204a6c26013be2018fd2015045b0f11a8b0
Santiago548 <dominicbarajas548@gmail.com>
2021-07-19 16:30:14 -0600

    testing overflow of profile picture on mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,8 @@ import {
   navLinkItem,
   navLinkText,
   siteTitle,
-  pageTop
+  pageTop,
+  navFloat,
 } from "./layout.module.css";
 const Layout = ({ pageTitle, children }) => {
   const data = useStaticQuery(graphql`
@@ -22,40 +23,41 @@ const Layout = ({ pageTitle, children }) => {
   return (
     <main className={container}>
       <div className={pageTop}>
-      <ul className={navLinks}>
-        <li className={navLinkItem}>
-          <Link to="/" className={navLinkText}>
-            HOME
-          </Link>
-        </li>
-        <li className={navLinkItem}>
-          <Link to="#about-page" className={navLinkText}>
-            ABOUTS
-          </Link>
-        </li>
-        <li className={navLinkItem}>
-          <Link to="#projects-page" className={navLinkText}>
-            PROJECTS
-          </Link>
-        </li>
-        <li className={navLinkItem}>
-          <Link to="#skills-page" className={navLinkText}>
-            SKILLS
-          </Link>
-        </li>
-        <li className={navLinkItem}>
-          <Link to="#contact-page" className={navLinkText}>
-            CONTACT
-          </Link>
-        </li>
-      </ul>
+        <div className={navFloat}>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                HOME
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="#about-page" className={navLinkText}>
+                ABOUT
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="#projects-page" className={navLinkText}>
+                PROJECTS
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="#skills-page" className={navLinkText}>
+                SKILLS
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="#contact-page" className={navLinkText}>
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+        </div>
         <title>
           {pageTitle} | {data.site.siteMetadata.title}
         </title>
         <p className={siteTitle}>{data.site.siteMetadata.title}</p>
-        <br />
       </div>
-        <h1 className={heading}>{pageTitle}</h1>
+        <br />
         {children}
     </main>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,8 @@ const IndexPage = () => {
   return (
     <div>
       <Layout pageTitle="Home Page">
+        <br />
+        <br />
         <h4 className={homeDescrption}>
           My name is Dominic, welcome to my site. I'm constantly expanding my
           knowledge as a Full-Stack Developer by building applications and
